d576e19ea40319768a66a662d5eb948707795ee7
tus15481 <tus15481@temple.edu>
2024-08-29 23:24:30 -0400

    added Random.h and random.c files

---
+++ Random.h
@@ -0,0 +1,13 @@
+//
+//  Random.h
+//  CIS_3207_HW!
+//
+//  Created by william yang on 8/29/24.
+//
+
+#ifndef Random_h
+#define Random_h
+
+#include <stdio.h>
+
+#endif /* Random_h */
---
+++ random.c
@@ -0,0 +1,8 @@
+//
+//  Random.c
+//  CIS_3207_HW!
+//
+//  Created by william yang on 8/29/24.
+//
+
+#include "Random.h"
