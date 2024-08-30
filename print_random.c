9b9580b8b1bd21de43f0fbacfb37248893020957
tus15481 <tus15481@temple.edu>
2024-08-30 02:41:22 -0400

    created rand_string to use randchar

--- print_random.c
+++ print_random.c
@@ -2,7 +2,9 @@
 #include <stdlib.h>
 #include "Random.h"
 //Given Project code
-/*
+
+void rand_string(char *strings, size_t size);
+
 char* rand_string_alloc(size_t size)
 {
      char *s = malloc(size + 1);
@@ -12,5 +14,10 @@ char* rand_string_alloc(size_t size)
      return s;
 }
  
-
-*/
+void rand_string(char *strings, size_t size){
+    for (size_t i = size; i > 0; --i) {
+        *strings = randchar();
+        ++strings;
+    }
+    *strings = '\0';
+}
